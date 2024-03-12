04a82b61c73c26a19d30706ee7fdae39629b6765
Ricky Renaldi <109163100+rickrck@users.noreply.github.com>
2024-03-13 05:13:30 +0700

    menambahkan implementasi dengan state-based construction

---
+++ DoorMachine.cs
@@ -0,0 +1,34 @@
+﻿public class DoorMachine
+{
+    enum State
+    { Terkunci, Terbuka };
+
+    public static void Main()
+    {
+        State state = State.Terkunci;
+        string[] screenName = { "Terkunci", "Terbuka" };
+        while (true)
+        {
+            Console.WriteLine("Pintu sekarang: " + screenName[(int)state]);
+            Console.WriteLine("Masukkan Perintah : ");
+
+            string perintah = Console.ReadLine();
+            switch (state)
+            {
+
+                case State.Terkunci:
+                    if (perintah == "buka")
+                    {
+                        state = State.Terbuka;
+                    }
+                    break;
+                case State.Terbuka:
+                    if (perintah == "tutup")
+                    {
+                        state = State.Terkunci;
+                    }
+                    break;
+            }
+        }
+    }
+}
